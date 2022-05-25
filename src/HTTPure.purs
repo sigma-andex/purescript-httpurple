f3837b05636dff2859883e46139fcec54ed18859
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2022-05-25 19:11:39 +0100

    Reexport routing-duplex to simplify imports

--- src/HTTPure.purs
+++ src/HTTPure.purs
@@ -10,6 +10,9 @@ module HTTPurple
   , module HTTPurple.Response
   , module HTTPurple.Server
   , module HTTPurple.Status
+  , module Routing.Duplex
+  , module Routing.Duplex.Generic
+  , module Routing.Duplex.Generic.Syntax
   ) where
 
 import HTTPurple.Body (toBuffer, toStream, toString)
@@ -20,6 +23,9 @@ import HTTPurple.Path (Path)
 import HTTPurple.Query (Query)
 import HTTPurple.Request (Request, fullPath)
 import HTTPurple.Response (Response, ResponseM, accepted, accepted', alreadyReported, alreadyReported', badGateway, badGateway', badRequest, badRequest', conflict, conflict', continue, continue', created, created', emptyResponse, emptyResponse', expectationFailed, expectationFailed', failedDependency, failedDependency', forbidden, forbidden', found, found', gatewayTimeout, gatewayTimeout', gone, gone', hTTPVersionNotSupported, hTTPVersionNotSupported', iMUsed, iMUsed', imATeapot, imATeapot', insufficientStorage, insufficientStorage', internalServerError, internalServerError', lengthRequired, lengthRequired', locked, locked', loopDetected, loopDetected', methodNotAllowed, methodNotAllowed', misdirectedRequest, misdirectedRequest', movedPermanently, movedPermanently', multiStatus, multiStatus', multipleChoices, multipleChoices', networkAuthenticationRequired, networkAuthenticationRequired', noContent, noContent', nonAuthoritativeInformation, nonAuthoritativeInformation', notAcceptable, notAcceptable', notExtended, notExtended', notFound, notFound', notImplemented, notImplemented', notModified, notModified', ok, ok', partialContent, partialContent', payloadTooLarge, payloadTooLarge', paymentRequired, paymentRequired', permanentRedirect, permanentRedirect', preconditionFailed, preconditionFailed', preconditionRequired, preconditionRequired', processing, processing', proxyAuthenticationRequired, proxyAuthenticationRequired', rangeNotSatisfiable, rangeNotSatisfiable', requestHeaderFieldsTooLarge, requestHeaderFieldsTooLarge', requestTimeout, requestTimeout', resetContent, resetContent', response, response', seeOther, seeOther', serviceUnavailable, serviceUnavailable', switchingProtocols, switchingProtocols', temporaryRedirect, temporaryRedirect', tooManyRequests, tooManyRequests', uRITooLong, uRITooLong', unauthorized, unauthorized', unavailableForLegalReasons, unavailableForLegalReasons', unprocessableEntity, unprocessableEntity', unsupportedMediaType, unsupportedMediaType', upgradeRequired, upgradeRequired', useProxy, useProxy', variantAlsoNegotiates, variantAlsoNegotiates')
-import HTTPurple.Routes (type (<+>), combineRoutes, orElse, (<+>))
+import HTTPurple.Routes (type (<+>), combineRoutes, orElse, (<+>), mkRoute)
 import HTTPurple.Server (ServerM, serve)
 import HTTPurple.Status (Status)
+import Routing.Duplex (class RouteDuplexBuildParams, class RouteDuplexParams, RouteDuplex(..), RouteDuplex', as, boolean, buildParams, default, end, flag, int, many, many1, optional, param, params, parse, path, prefix, print, prop, record, rest, root, segment, string, suffix, (:=))
+import Routing.Duplex.Generic (noArgs, product, sum, (~), class GRouteDuplex, class GRouteDuplexCtr)
+import Routing.Duplex.Generic.Syntax (gsep, (/), gparams, (?))
