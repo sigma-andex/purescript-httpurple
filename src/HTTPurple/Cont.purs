f928e1b98891d7a2010d7f297a17a1f14f6ee19b
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2022-06-05 20:12:57 +0100

    Add documentation

--- src/HTTPurple/Cont.purs
+++ src/HTTPurple/Cont.purs
@@ -6,5 +6,6 @@ import Prelude
 
 import Control.Monad.Cont (ContT, runContT)
 
+-- | Run the continuation
 usingCont :: forall output m. Applicative m => ContT output m output -> m output
 usingCont = flip runContT pure
